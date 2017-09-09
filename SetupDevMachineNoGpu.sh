f9de6a97a0d48d8387b13c1f98c8425ec22470eb
Mark Gloudemans <mark@gloudemans.net>
2017-09-09 09:23:40 -0400

    Created

---
+++ SetupDevMachineNoGpu.sh
@@ -0,0 +1,17 @@
+# Bring ubuntu install up to date
+sudo apt-get update
+sudo apt-get upgrade
+
+# Install ffmpeg
+sudo apt-get install ffmpeg
+
+# Install tensorflow for python 3 without GPU suport
+sudo apt-get install python3-pip python3-dev python-virtualenv
+virtualenv --system-site-packages -p python3 tensorflow
+source ~/tensorflow/bin/activate
+easy_install -U pip
+pip3 install --upgrade tensorflow
+source ~/tensorflow/bin/activate
+
+# Install numpy and scipy
+sudo pip3 install numpy scipy
