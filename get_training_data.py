df0e873d66639d452f0fa35837118f478a103ad4
Mark Gloudemans <mark@gloudemans.net>
2017-09-09 11:51:49 -0400

    Update get_training_data.py

--- get_training_data.py
+++ get_training_data.py
@@ -4,7 +4,7 @@ import numpy as np
 def get_training_data(filename, x, y, span, count):
 
     fifo = np.zeros( (span+1,y,x,3), dtype=np.uint8)
-    tensor = n.zeros( (count,y,x,3), dtype=np.uint8)
+    tensor = np.zeros( (count,y,x,3), dtype=np.uint8)
     target = npzeros( (count), dtype=np.float32)
    
     frame_length = 3*x*y
