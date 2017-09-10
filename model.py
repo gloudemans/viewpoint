a9d4caff2d1bdc40db2d819c09056e2a11e5ec9c
Mark Gloudemans <mark@gloudemans.net>
2017-09-10 14:14:12 -0400

    Update model.py

--- model.py
+++ model.py
@@ -164,10 +164,10 @@ def main(_):
       k = (i*50) % count
       x_batch = tensor[k:k+50]
       y_batch = target[k:k+50]
-      if i % 100 == 0:
-        train_accuracy = accuracy.eval(feed_dict={
-            x: x_batch[0], y_: y_batch, keep_prob: 1.0})
-        print('step %d, training accuracy %g' % (i, train_accuracy))
+      #if i % 100 == 0:
+      #  train_accuracy = accuracy.eval(feed_dict={
+      #      x: x_batch[0], y_: y_batch, keep_prob: 1.0})
+      #  print('step %d, training accuracy %g' % (i, train_accuracy))
       train_step.run(feed_dict={x: x_batch, y_: y_batch, keep_prob: 0.5})
 
     print('test accuracy %g' % accuracy.eval(feed_dict={
